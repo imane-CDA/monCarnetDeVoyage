4c88553935d4aa6e27a3f4e6885927426f5c95aa
imane-CDA <boujnane.imaane@gmail.com>
2026-06-03 19:20:33 +0200

    Initial commit - monCarnetDeVoyage

---
+++ monCarnetDeVoyage/monCarnetDeVoyage/monCarnetDeVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  monCarnetDeVoyageApp.swift
+//  monCarnetDeVoyage
+//
+//  Created by Apprenant131 on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct monCarnetDeVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
